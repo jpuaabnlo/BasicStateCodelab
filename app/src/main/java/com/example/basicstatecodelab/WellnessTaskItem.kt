88173ff30ec2a8972088839f7c3bae8f03cdacfe
jpuaabnlo <juanito_pablito545@hotmail.com>
2026-09-23 22:49:15 -0600

    List and Lazy List

--- app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -7,7 +7,13 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,6 +21,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ){
@@ -26,8 +34,25 @@ fun WellnessTaskItem(
             modifier = Modifier.weight(1f).padding(start = 16.dp),
             text = taskName
         )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
+}
+
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier){
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = {newValue -> checkedState = newValue},
+        onClose = {},
+        modifier = modifier
+    )
 }
---
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
@@ -0,0 +1,26 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+
+
+@Composable
+fun WellnessTaskList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks() }
+){
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) {
+            task -> WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
+
+fun getWellnessTasks() =
+    List(30) {
+        i -> WellnessTask(i, "Task # $i")}
